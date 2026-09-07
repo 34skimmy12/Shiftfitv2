a4707795072557a954c28d160d14e2a9ea75509a
34skimmy12 <108073523+34skimmy12@users.noreply.github.com>
2026-09-08 00:41:44 +0100

    Add Label primitive

---
+++ src/components/ui/label.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export function Label({ className="", children, ...props }) { return <label className={`text-sm font-medium leading-none ${className}`} {...props}>{children}</label>; }
